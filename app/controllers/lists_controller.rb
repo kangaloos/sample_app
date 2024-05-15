00fe318377f274983da9ac2a564fda70af14e47a
kangaloos <166692206+kangaloos@users.noreply.github.com>
2024-05-15 15:09:09 +0000

    5章【編集機能を作ろう】

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,17 +1,17 @@
 class ListsController < ApplicationController
   def new
-    # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+   
     @list = List.new
   end
   
-  # 以下を追加
+ 
   def create
-     # １.&2. データを受け取り新規登録するためのインスタンス作成
+     
       list = List.new(list_params)
-     # 3. データをデータベースに保存するためのsaveメソッド実行
+    
      list.save
      
-    # 詳細画面へリダイレクト
+    
     redirect_to list_path(list.id)
   end
  
@@ -26,7 +26,15 @@ class ListsController < ApplicationController
   end
 
   def edit
+     @list = List.find(params[:id])
   end
+  
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)  
+  end
+
   
   private
    # ストロングパラメータ
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   get 'lists/new'
    get '/top' => 'homes#top'
-   post 'lists' => 'lists#create'   #ここを追記します
+   post 'lists' => 'lists#create'  
  get 'lists' => 'lists#index'
-  get 'lists/edit'
-   # .../lists/1 や .../lists/3 に該当する
    get 'lists/:id' => 'lists#show', as: 'list'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+   get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
+  end
